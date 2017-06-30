60e9855a8349bcb1246f2bf65d78077356883e7e
meckblack <meckydrix@gmail.com>
2017-06-30 14:26:43 +0100

    Added DataTable Plugins

--- IMS/DAL/IMS_DB.cs
+++ IMS/DAL/IMS_DB.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Linq;
-using System.Web;
 using IMS.Models;
 
 namespace IMS.DAL
@@ -15,13 +11,14 @@ namespace IMS.DAL
             Database.SetInitializer<IMS_DB>(new MyInitializer());
         }
 
+        public DbSet<AppUser> AppUser { get; set; }
         public DbSet<Admin> admin { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Product> Product { get; set; }
-        public DbSet<Purchase> Purchase { get; set; }
+        public DbSet<Purchases> Purchases { get; set; }    
         public DbSet<Sale> Sale { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
